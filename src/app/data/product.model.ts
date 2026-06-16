f3abc413e74062f7365037881d4ef92238cc667d
piotkattt <ngoctam.chsae121@gmail.com>
2026-06-16 19:22:43 +0700

    cập nhật model theo ERD, tạo 20 sp mockup, các file component xóa thuộc tính tags (vì model ko có)

--- src/app/data/product.model.ts
+++ src/app/data/product.model.ts
@@ -1,12 +1,17 @@
 export interface Product {
   id: string;
   name: string;
+  brand: string;
   category: string;
-  price: number;
+  description: string;
   image: string;
-  tags: string[];
+  volume: string;
+  routineStep: string;
+  price: number;
+  quantity?: number;
+  stockQuantity: number;
+  targetSkinTypes: string[];
+  targetSkinProblems: string[];
   rating: number;
   reviews: number;
-  description: string;
-  quantity?: number;
 }
