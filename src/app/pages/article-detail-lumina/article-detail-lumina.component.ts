2fe02b23ba9c1125d040491ee3444b3a1097fff7
lekhoa1102 <lekimkhoa1102@gmail.com>
2026-07-10 21:09:49 +0700

    Khoa da sua lai KCN va Lumina

--- src/app/pages/article-detail-lumina/article-detail-lumina.component.ts
+++ src/app/pages/article-detail-lumina/article-detail-lumina.component.ts
@@ -15,7 +15,7 @@ export class ArticleDetailLuminaComponent implements OnInit {
   article = {
     title: 'Tinh Hoa Dưỡng Sáng Lumina',
     excerpt: 'Bộ sưu tập Lumina được thiết kế để mang lại ánh sáng tự nhiên cho làn da, với công thức độc quyền kết hợp các chiết xuất từ hoa hồng và vàng colloidal.',
-    image: 'https://lh3.googleusercontent.com/aida-public/AB6AXuB3TGHEBLX0pS9Ay6XVoyJtbAtfj-C4ssGB2kAeTlIvD6feY1hh0bngtp7sB_S3DCHDsQJLO9ZklmTPgW6HzpndSnR1JmcjyrRYczU7_xewXdS4lljjcZhVS3YM9_J7n9SAUB0il7hHly-KiDbA0hxYJS8NFQzz2xc6WWCHxYis8Jq3gJdXyzM2nLIBKMItYtib-wWLqG2h5ZKMKYaKb_uaOH1AT6erbQ84bR2XFpSvIHJLrxhhO3NWZsq4XGsXvvijh_AcqoaYz2UJ',
+    image: 'assets/images/Banner/LuminaBanner.jpg',
     collection: 'Lumina'
   };
   relatedProducts: Product[] = [];
